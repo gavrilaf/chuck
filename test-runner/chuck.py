9cf59e516f5f91394bacf85c2d06f1b66220c441
eugenf <eugenf78@gmail.com>
2019-03-28 19:26:37 +0200

    updated integration tests

--- test-runner/chuck.py
+++ test-runner/chuck.py
@@ -16,6 +16,7 @@ class ChuckTester:
         self.s.headers = {'aadhi-identifier': id}
         # print("Activate scenario {}, result {}".format(scenario, resp))
 
+    # scenario 1
     def auth_init(self):
         resp = self.s.get(self.endpoint + "/v1/authinit?format=json&apikey=1234567&code=7654321")
         if resp.status_code == 200:
@@ -36,6 +37,32 @@ class ChuckTester:
             return resp.json()
         else:
             return None
+
+    # scenario 2
+    def guest_auth(self):
+        resp = self.s.post(self.endpoint + "/v2/authtoken/guest?format=json&apikey=1234")
+        return resp.status_code
+
+    def delete_token(self):
+        resp = self.s.delete(self.endpoint + "/v1/authtoken/user-id-111?format=json&apikey=2345")
+        return resp.status_code
+
+    # scenario 3
+    def auth_init_sc3(self):
+        resp = self.s.get(self.endpoint + "/v1/authinit?format=json&apikey=1234567&code=7654321")
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            return None
+
+    def preferred_store(self):
+        resp = self.s.get(self.endpoint + "/v1/customer/profile/preferredstore?format=json&apikey=9878")
+        if resp.status_code == 200:
+            return resp.json()
+        else:
+            return None
+
+    # invalid requests
 
     def wrong_path(self):
         resp = self.s.post(self.endpoint + "/v2/login?format=json&apikey=987654")
--- test-runner/test_chuck.py
+++ test-runner/test_chuck.py
@@ -5,29 +5,53 @@ class BaseTests(unittest.TestCase):
 
     def setUp(self):
         self.api = ChuckTester()
-        self.api.activate_scenario("scenario-1", "id-1")
 
     def test_auth_init(self):
+        self.api.activate_scenario("scenario-1", "id-1")
         json = self.api.auth_init()
         self.assertEqual(json["code"], "this-is-very-secret-code")
 
     def test_verify(self):
+        self.api.activate_scenario("scenario-1", "id-1")
         json = self.api.verify()
         self.assertEqual(json["code"], "this-is-other-secret-code")
 
     def test_login(self):
+        self.api.activate_scenario("scenario-1", "id-1")
         json = self.api.login()
         self.assertEqual(json["accessToken"], "access-token")
         self.assertEqual(json["refreshToken"], "refresh-token")
         self.assertEqual(json["refreshTokenExpiresIn"], 1209599)
 
     def test_wrong_url(self):
+        self.api.activate_scenario("scenario-1", "id-1")
         code = self.api.wrong_path()
         self.assertEqual(code, 404)
 
     def test_wrong_query(self):
+        self.api.activate_scenario("scenario-1", "id-1")
         code = self.api.wrong_query()
         self.assertEqual(code, 404)
+
+    def test_guest_auth(self):
+        self.api.activate_scenario("scenario-2", "id-2")
+        code = self.api.guest_auth()
+        self.assertEqual(code, 200)
+
+    def test_delete_token(self):
+        self.api.activate_scenario("scenario-2", "id-2")
+        code = self.api.delete_token()
+        self.assertEqual(code, 200)
+
+    def test_auth_init_sc3(self):
+        self.api.activate_scenario("scenario-3", "id-3")
+        json = self.api.auth_init_sc3()
+        self.assertEqual(json["code"], "scenario-3-code")
+
+    def test_preferred_store(self):
+        self.api.activate_scenario("scenario-3", "id-3")
+        json = self.api.preferred_store()
+        self.assertEqual(json["Store"]["Name"], "store-name")
 
 
 
